4e7f228dbee89a89168141b58703e4e6c0a7d80e
canattmaca <119670826+canattmaca@users.noreply.github.com>
2023-04-10 07:44:13 +0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fee73ac53emshe33ffdeb776bfc7p14cb23jsn205016e33e07',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL='https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+
+export const WEATHER_API_URL='https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY ='3dc5b65558e121655d7e826e45777197'
